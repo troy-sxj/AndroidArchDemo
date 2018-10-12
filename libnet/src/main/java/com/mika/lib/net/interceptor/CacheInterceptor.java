4e75f6cb1ab3aae2d0c38c12d72ffac326d03397
mika <troyshangxj@gmail.com>
2018-10-12 17:35:05 +0800

    [Feature] 添加缓存拦截器

--- libnet/src/main/java/com/mika/lib/net/interceptor/CacheInterceptor.java
+++ libnet/src/main/java/com/mika/lib/net/interceptor/CacheInterceptor.java
@@ -1,13 +1,36 @@
 package com.mika.lib.net.interceptor;
 
+import android.text.TextUtils;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
+/**
+ * 缓存拦截器
+ * <p>
+ *     1. 需要缓存的接口上添加自定义头：Cache-Time
+ *     2. 根据Cache-Time指定的值作为缓存时长：ep 24*60*60 //缓存一天
+ * </p>
+ */
 public class CacheInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
-        return null;
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+        String cache = request.header("Cache-Time");
+        if (!TextUtils.isEmpty(cache)) {//缓存时间不为空
+            Response response1 = response.newBuilder()
+                    .removeHeader("Pragma")
+                    .removeHeader("Cache-Control")
+                    //cache for cache seconds
+                    .header("Cache-Control", "max-age="+cache)
+                    .build();
+            return response1;
+        } else {
+            return response;
+        }
     }
 }
